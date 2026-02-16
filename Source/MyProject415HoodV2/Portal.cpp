0b8d60bcd29e24ffd9d7b65373f5aaa6e22838ed
SNHUstuden101 <likeihood@yahoo.com>
2026-02-15 19:27:55 -0600

    Finished

--- Source/MyProject415HoodV2/Portal.cpp
+++ Source/MyProject415HoodV2/Portal.cpp
@@ -1,40 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Portal.h"
-#include "MyProject415HoodV2Character.h"
-#include "Kismet/GameplayStatics.h"
+#include "Portal.h" // Include the header file for the portal class to implement its functions
+#include "MyProject415HoodV2Character.h" // Include the header file for the player character class to access its properties and functions
+#include "Kismet/GameplayStatics.h" // Include the GameplayStatics header to access functions for getting the player's camera manager and other utility functions
 
 // Sets default values
 APortal::APortal()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-	mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	boxComp = CreateDefaultSubobject<UBoxComponent>("Box Comp");
-	sceneCapture = CreateDefaultSubobject<USceneCaptureComponent2D>("Capture");
-	rootArrow = CreateDefaultSubobject<UArrowComponent>("Root Arrow");
+	PrimaryActorTick.bCanEverTick = true; // Enable ticking for this actor
+	mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh"); // Initialize the static mesh component
+	boxComp = CreateDefaultSubobject<UBoxComponent>("Box Comp"); // Initialize the box component
+	sceneCapture = CreateDefaultSubobject<USceneCaptureComponent2D>("Capture"); // Initialize the scene capture component
+	rootArrow = CreateDefaultSubobject<UArrowComponent>("Root Arrow");	// Initialize the arrow component
 
-	RootComponent = boxComp;
-	mesh->SetupAttachment(boxComp);
-	sceneCapture->SetupAttachment(mesh);
-	rootArrow->SetupAttachment(RootComponent);
+	RootComponent = boxComp; // Set the box component as the root component of the actor
+	mesh->SetupAttachment(boxComp); // Attach the mesh to the box component
+	sceneCapture->SetupAttachment(mesh); // Attach the scene capture to the mesh component
+	rootArrow->SetupAttachment(RootComponent); // Attach the arrow to the root component
 
-	mesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+	mesh->SetCollisionResponseToAllChannels(ECR_Ignore); // Set the collision response of the mesh to ignore all channels so that it doesn't interfere with the player's movement
 
 }
 
 // Called when the game starts or when spawned
-void APortal::BeginPlay()
+void APortal::BeginPlay() // Override the BeginPlay function to bind the overlap event and set the material of the portal mesh
 {
 	Super::BeginPlay();
 	boxComp->OnComponentBeginOverlap.AddDynamic(this, &APortal::OnOverlapBegin); //Bind the overlap event to the OnOverlapBegin function
-	mesh->SetHiddenInSceneCapture(true);
+	mesh->SetHiddenInSceneCapture(true); // Set the mesh to be hidden in the scene capture so that it doesn't render itself in the portal view
 
 
-	if (mat)
+	if (mat) // If a material is assigned to the portal, set it to the mesh
 	{
-		mesh->SetMaterial(0, mat);
+		mesh->SetMaterial(0, mat); // Set the material of the mesh to the assigned material
 	}
 }
 
@@ -42,26 +42,26 @@ void APortal::BeginPlay()
 void APortal::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	UpdatePortals();
+	UpdatePortals(); // Call the UpdatePortals function every frame to update the position and rotation of the scene capture component based on the player's camera and the location of the other portal
 }
 
-void APortal::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void APortal::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) // This function is called when the player enters the box component of the portal, and it teleports the player to the location of the other portal if they are not already teleporting
 {
-	AMyProject415HoodV2Character* playerChar = Cast<AMyProject415HoodV2Character>(OtherActor);
+	AMyProject415HoodV2Character* playerChar = Cast<AMyProject415HoodV2Character>(OtherActor); // Cast the other actor to the player character class to check if it's the player who entered the portal
 
-	if (playerChar)
+	if (playerChar) // If the cast is successful, it means the player entered the portal
 	{
-		if (OtherPortal)
+		if (OtherPortal) // Check if the other portal reference is valid before trying to teleport the player
 		{
-			if (!playerChar->isTeleporting)
+			if (!playerChar->isTeleporting) // Check if the player is not already teleporting to prevent multiple teleports in quick succession
 			{
 				playerChar->isTeleporting = true; // Set the flag to prevent multiple teleports
-				FVector loc = OtherPortal->rootArrow->GetComponentLocation();
+				FVector loc = OtherPortal->rootArrow->GetComponentLocation(); // Get the location of the other portal's arrow component to use as the teleport destination
 				playerChar->SetActorLocation(loc); // Teleport the player to the other portal's location
 
-				FTimerHandle TimerHandle;
-				FTimerDelegate TimerDelegate;
-				TimerDelegate.BindUFunction(this, "SetBool", playerChar);
+				FTimerHandle TimerHandle; // Create a timer handle to manage the timer for resetting the teleporting flag
+				FTimerDelegate TimerDelegate; //	Create a timer delegate to bind the function that will reset the teleporting flag after a short delay
+				TimerDelegate.BindUFunction(this, "SetBool", playerChar); // Bind the SetBool function to the timer delegate, passing the player character as a parameter
 				GetWorld()->GetTimerManager().SetTimer(TimerHandle, TimerDelegate, 1, false); // Set a timer to reset the teleporting flag after 0.5 seconds
 
 			}
--- Source/MyProject415HoodV2/Portal.h
+++ Source/MyProject415HoodV2/Portal.h
@@ -44,21 +44,21 @@ UPROPERTY(EditAnywhere, BlueprintReadWrite) // The render target that will be us
 	UTextureRenderTarget2D* renderTarget;
 
 UPROPERTY(EditAnywhere)
-	UBoxComponent* boxComp;
+	UBoxComponent* boxComp; // The box component that will be used to detect when the player enters the portal
 
 UPROPERTY(EditAnywhere)
-	APortal* OtherPortal;
+	APortal* OtherPortal; // A reference to the other portal that this portal is linked to
 
 UPROPERTY(EditAnywhere)
-	UMaterialInterface* mat;
+	UMaterialInterface* mat; // The material that will be applied to the portal mesh
 
 UFUNCTION()
 void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 UFUNCTION()
-void SetBool(AMyProject415HoodV2Character* playerChar);
+void SetBool(AMyProject415HoodV2Character* playerChar); // A function to set the isTeleporting flag on the player character
 
 UFUNCTION()
-void UpdatePortals();
+void UpdatePortals(); // A function to update the scene capture component's position and rotation to match the other portal's position and rotation, so that it renders the correct view
 
 };
